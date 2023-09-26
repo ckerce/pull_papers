e1403a4f32f2d33e2f50a2f6ba82f853c32c96f1
ckerce <ckerce@gmail.com>
2023-09-26 10:08:51 -0400

    added abstract extraction
    Had a problem with seleniums wait function and it was a pain to debug.  Ugly patch-over to get it to run.

--- scrape_sel.py
+++ scrape_sel.py
@@ -5,6 +5,8 @@ import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+import pdb
+
 def extract_papers_selenium(url):
     # Initialize the Chrome WebDriver
     driver = webdriver.Chrome()
@@ -24,24 +26,15 @@ def extract_papers_selenium(url):
     for paper_entry in driver.find_elements(By.XPATH, '//div[@class="note  "]'):
         # Extract title
         title = paper_entry.find_element(By.XPATH, './/h4/a[1]').text.strip()
-#        print("Testing TITLE")
-#        print(f"Title: {title}")
-#        print(title)
         
         # Extract authors
         authors = [author.text.strip() for author in paper_entry.find_elements(By.XPATH, './/div[@class="note-authors"]/span/a')]
-#        print(f"Authors: {authors}")
-#        print(authors)
         
         # Extract date and conference
         meta_info = paper_entry.find_elements(By.XPATH, './/ul[@class="note-meta-info list-inline"]/li')
         date = meta_info[0].text.strip()
-#        print(f"Date: {date}")
-#        print(date)
 
         conference = meta_info[1].text.strip()
-#        print(f"Conference: {conference}")
-#        print(conference)
 
         # Try to extract PDF URL
         try:
@@ -55,18 +48,28 @@ def extract_papers_selenium(url):
         forum_link = paper_entry.find_element(By.XPATH, './/h4/a[1]').get_attribute("href")
         subdriver = webdriver.Chrome()
         try:
+           #breakpoint()
            subdriver.get(forum_link)
            #time.sleep(3)
            #abstract = "TO DO: write a function for this"
            # Wait until the abstract element is available
-           element_present = EC.presence_of_element_located((By.XPATH, '//div[@class="note-content"]/div/strong[text()="Abstract:"]/following-sibling::span'))
-           WebDriverWait(subdriver, 10).until(element_present)
+           #element_present = EC.presence_of_element_located((By.XPATH, '//div[@class="note-content"]/div/strong[text()="Abstract:"]/following-sibling::span'))
+           #WebDriverWait(subdriver, 10).until(element_present)
            # TODO: the following does not correctly pull out the abstract
-           abstract_text = subdriver.find_element(By.XPATH, '//div[@class="note-content"]/div/strong[text()="Abstract:"]/following-sibling::span').text
+           #abstract_text = subdriver.find_element(By.XPATH, '//strong[text()="Abstract:"]/following-sibling::*').text
+           #abstract_span = subdriver.find_element(By.XPATH, '//strong[text()="Abstract:"]/following-sibling::span[1]')
+           #abstract_text = abstract_span.text.strip()
+           #abstract_text = subdriver.find_element(By.XPATH, '//div[@class="note-content"]/div/strong[text()="Abstract:"]/following-sibling::span').text
+           time.sleep(5)
+           page_source = subdriver.page_source
+           abstract_idx = page_source.find("Abstract:")
+           abstract_text = page_source[abstract_idx+10:page_source.find("</div", abstract_idx)]
+           #pdb.set_trace()
            subdriver.quit()
         except:
            abstract_text = "Abstract retrieval failed"
 
+        #print(abstract_text)
         # Store the extracted information in the specified format
         extracted_items.append({
             'title': title,
@@ -85,7 +88,7 @@ def extract_papers_selenium(url):
 # URL of the webpage to scrape
 
 search_term="logic"
-for k in range(9,10):
+for k in range(1,10):
    url = "https://openreview.net/search?content=all&group=ICLR.cc&page="+str(k)+"&source=forum&term="+search_term
    #url = "https://openreview.net/search?content=all&group=NeurIPS.cc&page="+str(k)+"&source=forum&term="+search_term
    
